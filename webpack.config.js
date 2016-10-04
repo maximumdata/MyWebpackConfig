779e73c9b258b83f4e2432d26a4c93f7ad7b1df6
Mike Dettmer <mike@mikedettmer.com>
2016-10-04 11:29:34 -0400

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.sass/,
+        exclude: /node_modules/,
+        loader: 'style!css!sass'
+      }
+    ]
+  }
+}
